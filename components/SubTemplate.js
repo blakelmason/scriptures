518e23bc903a9b2a7ec774c8a4eab8d8e7a4beb8
Blake Mason <blakelmason@gmail.com>
2020-04-27 22:02:26 -0700

    commit

--- components/SubTemplate.js
+++ components/SubTemplate.js
@@ -5,7 +5,10 @@ export default function SubTemplate({ children }) {
   const router = useRouter()
   const split = router.pathname.split('/')
   const path = split[1]
-  if (split.length > 2 && path === 'scriptures')
-    return <Chapter>{children}</Chapter>
-  else return children
+  if (split.length > 2) {
+    return {
+      scriptures: <Chapter>{children}</Chapter>,
+      topics: <div>MAKE A TEMPLATE{children}</div>,
+    }[path]
+  } else return children
 }
