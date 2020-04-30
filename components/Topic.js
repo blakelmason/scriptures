c1606c9ccc7e73711d6d8b2876a4fc7912722615
Blake Mason <blakelmason@gmail.com>
2020-04-29 23:45:49 -0700

    commit

--- components/Topic.js
+++ components/Topic.js
@@ -5,12 +5,12 @@ import find from 'lodash/find'
 export default function Topic({ children }) {
   const { pathname } = useRouter()
   const topic = pathname.split('/')[2].split('-').join(' ').toUpperCase()
-  const upper = find(topics, ({ letter }) => letter === topic[0]).topics.find(
+  const title = find(topics, ({ letter }) => letter === topic[0]).topics.find(
     (name) => name.toUpperCase() === topic
   )
   return (
     <div>
-      <h4>{upper}</h4>
+      <h4>{title}</h4>
       <hr />
       {children}
     </div>
